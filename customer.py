cfc0562e63e05a01c17cd5cf586184934584b22a
kikikrittamate <krittamate.ch@ku.th>
2021-10-16 21:21:49 +0700

    move method

--- customer.py
+++ customer.py
@@ -1,6 +1,5 @@
 from rental import Rental
 from movie import Movie
-import logging
 
 
 class Customer:
@@ -51,36 +50,6 @@ class Customer:
         statement += "Frequent Renter Points earned: {}\n".format(frequent_renter_points)
 
         return statement
-
-    def get_amount(self):
-        # compute rental change
-        amount = 0
-        if self.get_movie().get_price_code() == Movie.REGULAR:
-            # Two days for $2, additional days 1.50 each.
-            amount = 2.0
-            if self.get_days_rented() > 2:
-                amount += 1.5 * (self.get_days_rented() - 2)
-        elif self.get_movie().get_price_code() == Movie.CHILDREN:
-            # Three days for $1.50, additional days 1.50 each.
-            amount = 1.5
-            if self.get_days_rented() > 3:
-                amount += 1.5 * (self.get_days_rented() - 3)
-        elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-            # Straight per day charge
-            amount = 3 * self.get_days_rented()
-        else:
-            log = logging.getLogger()
-            log.error(f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
-        return amount
-
-    def get_frequent_renter_points(self):
-        # award renter points
-        frequent_renter_points = 0
-        if self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-            frequent_renter_points += self.get_days_rented()
-        else:
-            frequent_renter_points += 1
-        return frequent_renter_points
 
 
 if __name__ == "__main__":
