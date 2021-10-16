dc581dd2ccd81934ab736d8bebc55297b77beccd
kikikrittamate <krittamate.ch@ku.th>
2021-10-17 03:02:47 +0700

    implement strategy using Enum

--- rental.py
+++ rental.py
@@ -1,58 +1,79 @@
+from enum import Enum
+
 from movie import Movie
 import logging
 
 
+class PriceCode(Enum):
+    """An enumeration for different kinds of movies and their behavior"""
+
+    new_release = {"price": lambda days: 3.0 * days,
+                   "frp": lambda days: days
+                   }
+    normal = {"price": lambda days: 2.0 + max((1.5 * (days-2)), 0),
+              "frp": lambda days: 1
+              }
+    children = {"price": lambda days: 1.5 + max((1.5 * (days-3)), 0),
+                "frp": lambda days: 1
+                }
+
+    def price(self, days: int) -> float:
+        "Return the rental price for a given number of days"""
+        pricing = self.value["price"]    # the enum member's price formula
+        return pricing(days)
+
+
 class Rental:
-	"""
-	A rental of a movie by customer.
-	From Fowler's refactoring example.
+    """
+    A rental of a movie by customer.
+    From Fowler's refactoring example.
 
-	A realistic Rental would have fields for the dates
-	that the movie was rented and returned, from which the
-	rental period is calculated.
-	But for simplicity of the example only a days_rented
-	field is used.
-	"""
-	
-	def __init__(self, movie, days_rented): 
-		"""Initialize a new movie rental object for a movie	\
-		with known rental period (daysRented).
-		"""
-		self.movie = movie
-		self.days_rented = days_rented
+    A realistic Rental would have fields for the dates
+    that the movie was rented and returned, from which the
+    rental period is calculated.
+    But for simplicity of the example only a days_rented
+    field is used.
+    """
 
-	def get_movie(self):
-		return self.movie
+    def __init__(self, movie, days_rented):
+        """Initialize a new movie rental object for a movie	\
+        with known rental period (daysRented).
+        """
+        self.movie = movie
+        self.days_rented = days_rented
 
-	def get_days_rented(self):
-		return self.days_rented
+    def get_movie(self):
+        return self.movie
 
-	def get_price(self):
-		# compute rental change
-		amount = 0
-		if self.get_movie().get_price_code() == Movie.REGULAR:
-			# Two days for $2, additional days 1.50 each.
-			amount = 2.0
-			if self.get_days_rented() > 2:
-				amount += 1.5 * (self.get_days_rented() - 2)
-		elif self.get_movie().get_price_code() == Movie.CHILDREN:
-			# Three days for $1.50, additional days 1.50 each.
-			amount = 1.5
-			if self.get_days_rented() > 3:
-				amount += 1.5 * (self.get_days_rented() - 3)
-		elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-			# Straight per day charge
-			amount = 3 * self.get_days_rented()
-		else:
-			log = logging.getLogger()
-			log.error(f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
-		return amount
+    def get_days_rented(self):
+        return self.days_rented
 
-	def get_rental_points(self):
-		# award renter points
-		frequent_renter_points = 0
-		if self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-			frequent_renter_points += self.get_days_rented()
-		else:
-			frequent_renter_points += 1
-		return frequent_renter_points
+    def get_price(self):
+        # compute rental change
+        amount = 0
+        if self.get_movie().get_price_code() == Movie.REGULAR:
+            # Two days for $2, additional days 1.50 each.
+            amount = 2.0
+            if self.get_days_rented() > 2:
+                amount += 1.5 * (self.get_days_rented() - 2)
+        elif self.get_movie().get_price_code() == Movie.CHILDREN:
+            # Three days for $1.50, additional days 1.50 each.
+            amount = 1.5
+            if self.get_days_rented() > 3:
+                amount += 1.5 * (self.get_days_rented() - 3)
+        elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
+            # Straight per day charge
+            amount = 3 * self.get_days_rented()
+        else:
+            log = logging.getLogger()
+            log.error(f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
+        return amount
+
+    def get_renter_points(self):
+        # award renter points
+        frequent_renter_points = 0
+        if self.get_movie().get_price_code() == Movie.NEW_RELEASE:
+            frequent_renter_points += self.get_days_rented()
+        else:
+            frequent_renter_points += 1
+        return frequent_renter_points
