2995ab1dd9cded4b27fda626114c50edaaf74ae0
kikikrittamate <krittamate.ch@ku.th>
2021-11-06 22:31:18 +0700

    add attributes to movie

--- movie.py
+++ movie.py
@@ -7,17 +7,28 @@ class Movie:
     NEW_RELEASE = 1
     CHILDREN = 2
 
-    def __init__(self, title, price_code):
+    def __init__(self, title, year, genre, price_code):
         # Initialize a new movie.
         self.title = title
+        self.year = year
+        self.genre = genre
         self.price_code = price_code
+
+    def get_title(self):
+        # get the title
+        return self.title
+
+    def get_year(self):
+        # get the year
+        return self.year
+
+    def get_genre(self):
+        # get the genre
+        return self.genre
 
     def get_price_code(self):
         # get the price code
         return self.price_code
-
-    def get_title(self):
-        return self.title
 
     def __str__(self):
         return self.title
